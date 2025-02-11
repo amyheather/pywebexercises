98c572998491e6a4cd3b1132888cba4af63c9983
amyheather <a.heather2@exeter.ac.uk>
2025-02-11 15:49:20 +0000

    style(lint): add linting of .qmd

--- lint.sh
+++ lint.sh
@@ -2,3 +2,13 @@
 
 echo "Linting package..."
 pylint ./pywebexercises
+
+echo "Linting quarto pages..."
+for qmd_file in pages/*.qmd; do
+    base_name=$(basename "$qmd_file" .qmd)
+    quarto render "$qmd_file" --to ipynb
+    nbqa pylint "_site/pages/${base_name}.ipynb"
+    rm "_site/pages/${base_name}.ipynb"
+done
+
+echo "Linting complete."
