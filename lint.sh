2c761eaf3152be41bc85b9cbaa72e793517c3f47
amyheather <a.heather2@exeter.ac.uk>
2025-08-22 11:34:16 +0100

    style(lint): edited lint.sh

--- lint.sh
+++ lint.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 echo "Linting package..."
-pylint ./pywebexercises
+pylint src
 
 echo "Linting quarto pages..."
-for qmd_file in pages/*.qmd; do
-    base_name=$(basename "$qmd_file" .qmd)
-    quarto render "$qmd_file" --to ipynb
-    nbqa pylint "_site/pages/${base_name}.ipynb"
-    rm "_site/pages/${base_name}.ipynb"
-done
-
-echo "Linting complete."
+lintquarto -l pylint -p docs
